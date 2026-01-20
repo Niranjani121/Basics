a16f4933653428e4ea54e5ee1a0376101918c4c0
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-20 12:10:50 -0800

    File created NumCheck.java

---
+++ NumCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumCheck {
+    public static void main(String[] args)
+    {
+        Scanner sca=new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int num=sca.nextInt();
+
+        if(num>0){
+            System.out.println("Positive number");
+        }
+        else if(num<0){
+            System.out.println("Negative Number");
+        }
+        else{
+            System.out.println("The number is Zero");
+        }
+    }
+}
