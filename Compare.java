0ce7e546f41009e040262353f91f7a70d471894e
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-20 11:51:38 -0800

    File Created Compare.java

---
+++ Compare.java
@@ -0,0 +1,19 @@
+public class Compare{
+    public static void main(String[] args){
+        int num1=20;
+        int num2=30;
+
+        if(num1>num2)
+        {
+            System.out.println("num1 is greater than num2");
+        }
+        else if(num1<num2)
+        {
+            System.out.println("num2 is greater than num1");
+        }
+        else{
+            System.out.println("num1 is equal to num2");
+        }
+    }
+    
+}
