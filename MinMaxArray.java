9b37f607614fea37de710d99cbb39cadc6f17c04
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-20 15:31:54 -0800

    File created MinMaxArray.java

---
+++ MinMaxArray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MinMaxArray { 
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter array size: ");
+        int n=sc.nextInt();
+         int[] arr=new int[n];
+
+         for(int i=0;1<n;i++){
+            System.out.println("Enter element");
+            arr[i]=sc.nextInt();
+         }
+         int min=arr[0];
+         int max=arr[0];
+
+         for(int i=1;i<n;i++){
+            if(arr[i]<min)min=arr[i];
+            if(arr[i]>max)max=arr[i];
+         }
+         System.out.println("Smallest="+min);
+         System.out.println("Largest="+max);
+
+    }
+    
+}
