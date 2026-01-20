2af98a026daa19242742976c3a9fc2948da9e817
Niranjani121 <niranjaniseenivasan13@gmail.com>
2026-01-20 13:01:29 -0800

    File created Guess.java

---
+++ Guess.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Guess {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Random rand=new Random();
+
+        int number=rand.nextInt(10)+1;
+        int guess,count=0;
+
+        do{
+            System.out.println("Guess the number:");
+            guess=sc.nextInt();
+            count++;
+
+            if(guess>number)
+            {
+                System.out.println("Too High");
+            }
+            else if(guess<number){
+                System.out.println("Too Low");
+            }
+            else{
+                System.out.println("Correct Attempt"+count);
+            }
+        } while(guess!=number);
+    }
+}
